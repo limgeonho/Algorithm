8a6d07d44df639836d0e687772bac8f00a36c0f2
limgeonho <ghlim909@gmail.com>
2021-06-08 12:19:44 +0900

    BOJ

--- BOJ/BFS/13913.py
+++ BOJ/BFS/13913.py
@@ -1,29 +1,30 @@
+#숨바꼭질4
 from collections import deque
 
 def path(x):
     arr = []
     temp = x
-    for _ in range(dist[x]+1):
+    for _ in range(dist[x] + 1):
         arr.append(temp)
         temp = move[temp]
     print(' '.join(map(str, arr[::-1])))
 
 def bfs():
-    q = deque()
-    q.append(N)
-    while q:
-        x = q.popleft()
-        if x == K:
-            print(dist[x])
-            path(x)
-            return x
-        for i in (x+1, x-1, 2*x):
-            if 0<=i<=100000 and dist[i]==0:
-                q.append(i)
-                dist[i] = dist[x] + 1
-                move[i] = x
+    Q = deque()
+    Q.append(N)
+    while Q:
+        tmp = Q.popleft()
+        if tmp == K:
+            print(dist[tmp])
+            path(tmp)
+            return tmp
+        for i in (tmp+1, tmp-1, tmp*2):
+            if 0<= i < 100000 and dist[i] == 0:
+                Q.append(i)
+                dist[i] = dist[tmp] + 1
+                move[i] = tmp 
 
 N, K = map(int, input().split())
-dist = [0]*100001
-move = [0]*100001
+dist = [0] * 100001
+move = [0] * 100001
 bfs()
--- BOJ/BFS/1525.py
+++ BOJ/BFS/1525.py
@@ -1,45 +1,42 @@
+#퍼즐
 from collections import deque
 
 def bfs():
-    q=deque()
-    q.append(aa)
+    Q = deque()
+    Q.append(aa)
 
-    visited[aa]=0
-    while q:
-        v=q.popleft()
-        if v=='123456789':
+    visited[aa] = 0
+
+    while Q:
+        v = Q.popleft()
+        if v == '123456789':
             return visited[v]
         
-        s=str(v)
-        target=s.find('9')
-        tx=target//3
-        ty=target%3
+        s = str(v)
+        target = s.find('9')
+        tx = target//3
+        ty = target%3
 
         for i in range(4):
-            x=dx[i]+tx
-            y=dy[i]+ty
+            x = tx + dx[i]
+            y = ty + dy[i]
             if 0<=x<3 and 0<=y<3:
-                t=x*3+y
-                ts=list(s)
+                t = x * 3 + y
+                ts = list(s)
                 ts[target], ts[t] = ts[t], ts[target]
-                ti=int(''.join(ts))
+                ti = int(''.join(ts))
                 if not visited.get(ti):
-                    visited[ti]=visited[v]+1
-                    q.append(ti)
+                    visited[ti] = visited[v] + 1
+                    Q.append(ti)
     return -1
 
-
-
-
-
-
-dx=[-1,0,1,0]
-dy=[0,1,0,-1]
-visited=dict()
-aa=''
-for i in range(3):
-    a=input()
-    a=a.replace(' ','')
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+visited = dict()
+aa = ''
+for _ in range(3):
+    a = input()
+    a = a.replace(' ', '')
     aa+=a
 aa=int(aa.replace('0', '9'))
 print(bfs())
